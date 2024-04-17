fb04b7cd7cf0624eb6e4884237543ecf90240deb
Vinit Vyas <vinitvyas09@vinits-mbp.attlocal.net>
2024-04-17 00:13:29 -0700

    Adding API limiter code with prisma and planetscale db

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,6 +2,9 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -22,6 +25,12 @@ export async function POST(
             return new NextResponse("Prompt is required", { status: 400});
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if(!freeTrial){
+            return new NextResponse("Free trial has expired", { status: 403 });
+        }
+
         const input = {
             prompt: prompt,
             model_version: "stereo-large",
@@ -31,6 +40,8 @@ export async function POST(
         
         const response = await replicate.run("meta/musicgen:671ac645ce5e552cc63a54a2bbff63fcf798043055d2dac5fc9e36a837eedcfb", { input });
         console.log("HEREE response", response)
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
 
