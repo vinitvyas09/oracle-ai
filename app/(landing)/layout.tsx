7ef3b3abd2722d9d7e3e3b464f8db95dd0eb0caa
Vinit Vyas <vinitvyas09@vinits-mbp.attlocal.net>
2024-04-18 04:53:11 -0700

    Fix typescript p1

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,8 @@
-const LandingLayout = ({
-    children
-}) => {
+interface LandingLayoutProps {
+    children: React.ReactNode;
+}
+
+const LandingLayout: React.FC<LandingLayoutProps> = ({ children }) => {
     return (
         <main className="h-full bg-[#111827] overflow-auto">
             <div className="mx-auto max-w-screen-xl h-full w-full">
